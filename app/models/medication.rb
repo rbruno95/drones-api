8760cc0b7dbcaf160852661839ee4a8bc027f0d5
Bruno <48922045+rbruno95@users.noreply.github.com>
2022-05-19 01:34:36 -0400

    add medication validations and tests

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -1,2 +1,8 @@
 class Medication < ApplicationRecord
+  belongs_to :drone
+
+  validates :name, format: { with: /\A[\w\-]+\z/ }
+  validates :code, format: { with: /\A[A-Z\d_]+\z/ }
+  validates :weight, numericality: true
+  validates :name, :code, :weight, presence: true
 end
--- test/models/medication_test.rb
+++ test/models/medication_test.rb
@@ -1,7 +1,32 @@
 require "test_helper"
 
 class MedicationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @medication = medications(:valid)
+  end
+
+  test 'valid medication' do
+    assert @medication.valid?
+  end
+
+  test 'invalid name' do
+    @medication.name = 'abc@'
+
+    refute @medication.valid?
+    assert_not_nil @medication.errors[:name]
+  end
+
+  test 'invalid code' do
+    @medication.code = 'abcD'
+
+    refute @medication.valid?
+    assert_not_nil @medication.errors[:code]
+  end
+
+  test 'invalid does not have a drone' do
+    @medication.drone = nil
+
+    refute @medication.valid?
+    assert_not_nil @medication.errors[:drone_id]
+  end
 end
