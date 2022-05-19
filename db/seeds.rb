1da30eb91d18675d422e5019c3517eb896e60658
Bruno <48922045+rbruno95@users.noreply.github.com>
2022-05-19 01:51:02 -0400

    fix drone fields on seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,10 @@ Drone.delete_all
 
 10.times do
   drone = Drone.create!(
-    serial: Faker::Alphanumeric.alphanumeric(number: 100),
+    serial_number: Faker::Alphanumeric.alphanumeric(number: 100),
     model: Drone.models.values.sample,
     weight_limit: rand(400..500),
-    battery: rand(50..100),
+    battery_capacity: rand(50..100),
     state: Drone.states.values.sample
   )
 
