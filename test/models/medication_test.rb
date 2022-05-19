32b92b6f784ad65ceec005e3375bfe44d73a1cec
Bruno <48922045+rbruno95@users.noreply.github.com>
2022-05-19 01:52:45 -0400

    add-medications (#8)
    * add medication model and migration
    
    * add medication validations and tests
    
    * add drones validations related to medications
    
    * add medications to seed
    
    * add active_storage
    
    * add medication cover_image
    
    * add medication cover_image test
    
    * fix rubocop offenses
    
    * fix drone fields on seeds.rb

---
+++ test/models/medication_test.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class MedicationTest < ActiveSupport::TestCase
+  def setup
+    @medication = medications(:valid)
+  end
+
+  test 'valid medication' do
+    assert @medication.valid?
+  end
+
+  test 'valid with an image' do
+    image = File.open(Rails.root.join('test', 'fixtures', 'files', 'pills.png'))
+    @medication.cover_image.attach(io: image, filename: 'pills.png')
+
+    assert @medication.valid?
+    assert @medication.cover_image.attached?
+  end
+
+  test 'invalid name' do
+    @medication.name = 'abc@'
+
+    refute @medication.valid?
+    assert_not_nil @medication.errors[:name]
+  end
+
+  test 'invalid code' do
+    @medication.code = 'abcD'
+
+    refute @medication.valid?
+    assert_not_nil @medication.errors[:code]
+  end
+
+  test 'invalid does not have a drone' do
+    @medication.drone = nil
+
+    refute @medication.valid?
+    assert_not_nil @medication.errors[:drone_id]
+  end
+end
