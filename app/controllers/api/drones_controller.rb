6c73a26b06449486dc4f2ee5df3c870716a9a2db
Bruno <48922045+rbruno95@users.noreply.github.com>
2022-05-18 23:54:37 -0400

    add drone api endpoints, tests and routes

---
+++ app/controllers/api/drones_controller.rb
@@ -0,0 +1,27 @@
+module Api
+  class DronesController < ApplicationController
+    def index
+      render json: Drone.all
+    end
+
+    def show
+      render json: Drone.find(params[:id])
+    end
+
+    def create
+      drone = Drone.new(permitted_params)
+
+      if drone.save
+        render json: drone, status: :created
+      else
+        render json: { errors: drone.errors }, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def permitted_params
+      params.require(:drone).permit(:serial_number,:model, :weight_limit, :battery_capacity, :state)
+    end
+  end
+end
