3878862d47b5d8471d306ef4510869ecd7880955
Bruno <48922045+rbruno95@users.noreply.github.com>
2022-05-19 01:40:11 -0400

    add medications to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,20 @@
 Drone.delete_all
 
 10.times do
-  Drone.create!(
+  drone = Drone.create!(
     serial_number: Faker::Alphanumeric.alphanumeric(number: 100),
     model: Drone.models.values.sample,
     weight_limit: rand(400..500),
     battery_capacity: rand(50..100),
     state: Drone.states.values.sample
   )
+
+  2.times do
+    Medication.create!(
+      name: Faker::Alphanumeric.alphanumeric(number: 10),
+      weight: rand(2..50),
+      code: Faker::Code.asin,
+      drone: drone
+    )
+  end
 end
