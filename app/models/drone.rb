6abc4394ba279932ae4e1a2ee7ceac30346e23dc
Bruno <48922045+rbruno95@users.noreply.github.com>
2022-05-19 01:46:21 -0400

    fix rubocop offenses

--- app/models/drone.rb
+++ app/models/drone.rb
@@ -31,9 +31,7 @@ class Drone < ApplicationRecord
   private
 
   def can_carry_medications?
-    if medications.map(&:weight).sum > weight_limit
-      errors.add(:weight_limit, 'This drone cannot carry this medication due to its weight limit.')
-    end
+    errors.add(:weight_limit, 'This drone cannot carry this medication due to its weight limit.') if medications.map(&:weight).sum > weight_limit
   end
 
   def cannot_load_with_low_battery
