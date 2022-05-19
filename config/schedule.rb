16fc50c96bbe1ca4e7d72ea87938c177f1e5e468
Bruno <48922045+rbruno95@users.noreply.github.com>
2022-05-19 07:54:25 -0400

    add docker-compose.yml, change some configs

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,10 @@
 set :output, "log/cron.log"
 
-set :environment, "development"
+ENV.each_key do |key|
+  env key.to_sym, ENV[key]
+end
+
+set :environment, ENV["RAILS_ENV"]
 
 every :minute do
   rake "periodic_tasks:check_drones"
