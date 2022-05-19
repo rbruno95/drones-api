29006f936693601f04f1d4ce0442f8d27445efe4
Bruno <48922045+rbruno95@users.noreply.github.com>
2022-05-19 00:26:36 -0400

    add populate db script

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# frozen_string_literal: true
+
+Drone.delete_all
+
+10.times do
+  Drone.create!(
+    serial_number: Faker::Alphanumeric.alphanumeric(number: 100),
+    model: Drone.models.values.sample,
+    weight_limit: rand(400..500),
+    battery_capacity: rand(50..100),
+    state: Drone.states.values.sample
+  )
+end
